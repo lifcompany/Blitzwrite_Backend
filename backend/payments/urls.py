67a9cfbc53584e3419e736022c5744e3fbb783cd
santa116-1 <totoplife116@gmail.com>
2024-05-08 20:37:59 +0900

    add urls

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('payment/', views.payment_view, name='payment'),
+    path('create_payment_intent/', views.create_payment_intent, name='create_payment_intent'),
+    path('api/create-payment-intent/', views.create_payment_intent, name='create_payment_intent'),
+    path('api/stripe-webhook/', views.stripe_webhook, name='stripe_webhook'),
 ]

9c759202626e6db923e7067735137c74df0161f4
santa116-1 <totoplife116@gmail.com>
2024-05-13 11:14:34 +0900

    lint api

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import LoginView, RegisterView, GoogleRegister, GetUserView, MailVerifyView, ForgetPasswordView, ResetPasswordView, loginWithGoogle, GetUserInfo
+from .views import GetFileList, RegisterView, GoogleRegister, GetUserView, MailVerifyView, ForgetPasswordView, ResetPasswordView, loginWithGoogle, GetUserInfo
 
 urlpatterns = [
-    path("get_file_list/", view=LoginView.as_view(), name="login"),
-    path("get_model_list/", view=RegisterView.as_view(), name="register"),
-    path("add_new_version/", view=GoogleRegister.as_view(), name="googleregister"),
-    path("get_edit_version/", view=MailVerifyView.as_view(), name="mailverify"),
-    path("delete_model/", view=ForgetPasswordView.as_view(), name="forgetpassword"),
-    path("delete_files/", view=ResetPasswordView.as_view(), name="resetpassword"),
-    path("get_file_content/", view=GetUserView.as_view(), name="getUserInfo"),
-    path("post_article/", view=GetUserInfo.as_view(), name="getUserInfoData"),
-    path("run_script/", view=GetUserInfo.as_view(), name="getUserInfoData"),
-    path("stop_script/", view=loginWithGoogle.as_view(), name="loginWithGoogle"),
+    path("get_file_list/", view=GetFileList.as_view(), name="get_file_list"),
+    path("get_model_list/", view=RegisterView.as_view(), name="get_model_list"),
+    path("add_new_version/", view=GoogleRegister.as_view(), name="add_new_version"),
+    path("get_edit_version/", view=MailVerifyView.as_view(), name="get_edit_version"),
+    path("delete_model/", view=ForgetPasswordView.as_view(), name="delete_model"),
+    path("delete_files/", view=ResetPasswordView.as_view(), name="delete_files"),
+    path("get_file_content/", view=GetUserView.as_view(), name="get_file_content"),
+    path("post_article/", view=GetUserInfo.as_view(), name="post_article"),
+    path("run_script/", view=GetUserInfo.as_view(), name="run_script"),
+    path("stop_script/", view=loginWithGoogle.as_view(), name="stop_script"),
     path("/download/<filename>/", view=GetUserInfo.as_view(), name="downloadfile"),
 ]

634d44c1c8f48a6236ee330e6daa93e5005a882c
santa116-1 <totoplife116@gmail.com>
2024-07-24 01:58:35 +0900

    fix: RESET password main

--- backend/users/api/views.py
+++ backend/users/api/views.py
@@ -152,7 +152,7 @@ def send_reset_password(email, verification_link):
         <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Email Verification</title>
+        <title>パスワード再設定</title>
         <style>
             body {
             font-family: Arial, sans-serif;
